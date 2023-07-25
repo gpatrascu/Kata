e81e131bc09632224a5aec300810dc438fee8fb4
George-Patrascu <g.patrascu@criteo.com>
2023-07-25 14:50:03 +0300

    with queue

---
+++ lrucache/LRUCache.cs
@@ -0,0 +1,50 @@
+namespace lrucache;
+
+public class LRUCache
+{
+    private readonly int _capacity;
+    private readonly Dictionary<int, int> _dictionary;
+
+    readonly Queue<int> touched;
+    
+    public LRUCache(int capacity) {
+        _capacity = capacity;
+        _dictionary = new Dictionary<int, int>();
+        touched = new Queue<int>();
+    }
+    
+    public int Get(int key)
+    {
+        var val = _dictionary.TryGetValue(key, out var value) ? value : -1;
+
+        if (val == -1)
+        {
+            return -1;
+        }
+        
+        touched.Enqueue(key);
+        RemoveUnusedElements();
+
+        
+        return val;
+    }
+
+    private void RemoveUnusedElements()
+    {
+        if (touched.Count > _capacity)
+        {
+            var dequeue = touched.Dequeue();
+            if (!touched.Contains(dequeue))
+            {
+                _dictionary.Remove(dequeue);
+            }
+        }
+    }
+
+    public void Put(int key, int value) {
+        touched.Enqueue(key);
+        RemoveUnusedElements();
+
+        _dictionary.Add(key, value);
+    }
+}
